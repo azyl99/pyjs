be3804951bdab4c90041eee01162cdd0302f189e
zyw3140 <2375267940@qq.com>
2016-12-29 14:58:51 +0800

    Signed-off-by: zyw3140 <2375267940@qq.com>

--- lexer.js
+++ lexer.js
@@ -1,11 +1,12 @@
 
 /**
- * Created by 3140102511 on 2016/12/23.
- */
-
-/**
- * Created by 3140102511 on 2016/12/23.
- */
+ *lexer支持test.py里的所有程序
+ *if for while def class 结束后末尾会添上如end+if
+ *tokenlist里元素为二元数组，数组元素是值和行号
+ *lexer函数运行完毕tokenlist即被赋值,调用时只需lexer(inputString)即可
+ *数字负数、小数均支持（类调用时'.'会分隔出来成为一个单独的token）
+ *字符串目前只支持''和"",尚未支持多行字符串，字符串的内容里不可出现单引号和双引号，此外一切均支持，包括转义字符
+ *  */
 var linesIndex;
 var lines;
 var splitChar=/\s+/;
@@ -14,7 +15,7 @@ var operator=["<=",">=","==","!=","&&","||","+=","-=","*=","/="];
 var TokenType=["spli","oper","single","num","str"];
 var indentKey=["if","while","for","def","class"];
 
-function lexer(inputString) {//对程序进行词法分析，生成tokenList
+function lexer(inputString) {//对程序进行词法分析，生成tokenlist
     var handleString = addSplit(inputString);  //为某一个token加上间隔符；
     var testString = "";
     var testLinesNum;
@@ -28,7 +29,7 @@ function lexer(inputString) {//对程序进行词法分析，生成tokenList
     linesIndex = 0;
     indent([0,0],[-1,-1]);
     testLinesNum=lines.length;
-    tokenList = [];
+    tokenlist = [];
     for(linesIndex = 0; linesIndex<testLinesNum; linesIndex++){
         lineContent = lines[linesIndex];
         currentToken[0]="";
@@ -36,21 +37,21 @@ function lexer(inputString) {//对程序进行词法分析，生成tokenList
         for(i = 0; i<lineContent.length; i++){
             if(splitChar.test(lineContent[i]) && strSign == 0){
                 if(currentToken[0] != ""){
-                    tokenList.push([currentToken[0],currentToken[1]]);
+                    tokenlist.push([currentToken[0],currentToken[1]]);
                     currentToken[0]="";
                 }
             }
             else if(lineContent[i] == '\'' || lineContent[i] == '\"' && strSign ==0){
                 if(currentToken[0] != ""){
-                    tokenList.push([currentToken[0],currentToken[1]]);
+                    tokenlist.push([currentToken[0],currentToken[1]]);
                 }
                 currentToken[0] = lineContent[i];
-                tokenList.push([currentToken[0],currentToken[1]]);
+                tokenlist.push([currentToken[0],currentToken[1]]);
                 currentToken[0]="";
                 strSign = 1;
             }
             else if(lineContent[i] == '\'' || lineContent[i] == '\"' && strSign ==1){
-                tokenList.push([currentToken[0],currentToken[1]]);
+                tokenlist.push([currentToken[0],currentToken[1]]);
                 currentToken[0] = lineContent[i];
                 strSign = 0;
             }
@@ -59,14 +60,14 @@ function lexer(inputString) {//对程序进行词法分析，生成tokenList
             }
         }
         if(currentToken[0]!=""){
-            tokenList.push([currentToken[0],currentToken[1]]);
+            tokenlist.push([currentToken[0],currentToken[1]]);
         }
     }
 }
 
 function indent(start,last) {  //处理缩进，if,for,while 加end ,start数组第一个元素为等价空格数，第二个为字符数
     var currentKey = "", nextStart, nextIndentStart;
-    var firstToken;
+    var firstToken,firstToken2;
     nextStart = start;
     try {
         while (nextStart[0] >= start[0]) {
@@ -82,7 +83,35 @@ function indent(start,last) {  //处理缩进，if,for,while 加end ,start数组
                     else {
                         indent(nextIndentStart, nextStart);
                     }
-                    lines.splice(linesIndex, 0, "end" + firstToken);
+                    if(linesIndex<linesNum){
+                        nextStart = calBlankNum(lines[linesIndex]);
+                        firstToken2 = getStartOfline(lines[linesIndex].substring(nextStart[1]));
+                    }
+                    while(firstToken2 == "elif" || firstToken2 == "else"){
+                        linesIndex++;
+                        nextIndentStart = calBlankNum(lines[linesIndex]);
+                        if (nextIndentStart[0] <= nextStart[0]) {
+                            throw new Error("indent error");
+                        }
+                        else {
+                            indent(nextIndentStart, nextStart);
+                        }
+                        if(linesIndex<linesNum){
+                            nextStart = calBlankNum(lines[linesIndex]);
+                            firstToken2 = getStartOfline(lines[linesIndex].substring(nextStart[1]));
+                        }
+                        else{
+                            firstToken2="";
+                        }
+                    }
+                    if(linesIndex == linesNum){
+                        lines.push("end" + firstToken);
+                    }
+                    else{
+                        lines.splice(linesIndex, 0, "end" + firstToken);
+                    }
+                    linesIndex++;
+                    linesNum++;
                 }
                 else {
                     linesIndex++;
@@ -99,7 +128,6 @@ function indent(start,last) {  //处理缩进，if,for,while 加end ,start数组
         if (linesIndex < linesNum && nextStart[0] > last[0]) {
             throw new Error("indent error");
         }
-
     } catch (e) {
         alert(e.message);
     }
@@ -133,7 +161,7 @@ function getStartOfline(str){  //获得每行第一个token的长度
 
 function checkType(c){
     var specialChar1=['!','>','<','+','-','*','/','%','=','|','&'];
-    var specialChar2=[',','(',')',':',';','\'','\"'];
+    var specialChar2=[',','(',')',':',';','\'','\"','[',']','{','}'];
 
     if(splitChar.test(c)){
         return 0;
@@ -161,6 +189,7 @@ function addSplit(inputString){
         type: TokenType[0],
         content: ""
     };
+    var strcount;
     var strSign = 0;
     for (var i = 0; i < len; i++ ) {  //
         currentChar = checkType(inputString[i]);
@@ -213,7 +242,7 @@ function addSplit(inputString){
         else if(currentChar==3){
             if(i ==1 && inputString[i-1] == '-' || i>1 && inputString[i-1] == '-' && (checkType(inputString[i-2])<=2)){
                 handleString += inputString[i];
-                currentToken.type == TokenType[3];
+                currentToken.type = TokenType[3];
                 currentToken.content += inputString[i];
             }
             else if(currentToken.type == TokenType[0]){
