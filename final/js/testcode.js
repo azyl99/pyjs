72aa61f81d6edb739049e58aa309a652ff320c95
czm1220 <530537501@qq.com>
2017-01-02 13:38:41 +0800

    修改函数&类部分的测试代码
    Signed-off-by: czm1220 <530537501@qq.com>

--- final/js/testcode.js
+++ final/js/testcode.js
@@ -32,57 +32,85 @@ while x<10:
 
 var test_for = ``
 
-var test_function1 = `x=1
-y=2
+var test_function1 = `x = 1
+y = 2
+z = 3
 def fa(x):
+    z = 5
+    print("local:")
     print(x)
     print(y)
+    print(z)
 fa(4)
+print("global:")
+print(x)
+print(y)
+print(z)`
 
-x=[1,2]
-y=[3,4]
-def fa (x):
-  print(x)
-  print(y)
-fa(4)`
-
-var test_function2 = `def fc(x):
-  if x<=10:
-    x=fc(x+1)
-  return x
-b=fc(9)
+var test_function2 = `def fact(x):
+    if x <= 1:
+        return 1
+    else:
+        return x * fact(x-1)
+b = fact(4)
 print(b)`
 
-var test_class1 = ``
+var test_class1 = `class Student():
+    def __init__(self, name, school):
+        self.name = name
+        self.__school = school
+    def print(self, x):
+        print(self.name)
+        print(x)
+        print(self.__school)
+        
+bart = Student("bart","ZJU")
+bart.print("is a student in ")
+bart.name = "bart2"
+bart.print("is a student in ")`
 
 var test_class2 = `class Animal():
-  def __init__(self,name):
-    self.name=name
-  def f():
-    print("animal:f()")
-  def run(self):
-    print("animal is running")
+    def __init__(self, name):
+        self.name = name
+    def f():
+        print("animal:f()")
+    def run(self):
+        print("animal is running")
 
 class Dog(Animal):
-  def run(self):
-    print("dog is running")
+    def run(self):
+        print("dog is running")
 
 class Cat(Animal):
-  def __init__(self,name,hobby):
-    self.name=name
-    self.hobby=hobby
-  def run(self):
-    print("cat is running")
-	
-animal=Animal(10)
-dog=Dog("dog")
-cat=Cat("cat","ball")
+    def __init__(self,name,hobby):
+        self.name = name
+        self.hobby = hobby
+    def run(self):
+        print("cat is running")
+
+animal = Animal(10)
+dog = Dog("dog")
+cat = Cat("cat", "ball")
+
+print("variable:")
+print(dog.name)
+print(cat.hobby)
+
+print("f():")
 dog.f()
 cat.f()
+
+print("run():")
 dog.run()
 cat.run()
-print(dog.name)
-print(cat.hobby)`
+
+def run(animal):
+    animal.run()
+    
+print("run():")
+run(animal)
+run(dog)
+run(cat)`
 	
 test_list = ``
 
